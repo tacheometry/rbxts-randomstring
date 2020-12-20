812413b29e21054f7518df5b929f40a27ec5c2df
tacheometry <39647014+tacheometry@users.noreply.github.com>
2020-12-20 04:58:40 +0200

    Initial commit

--- out/index.d.ts
+++ out/index.d.ts
@@ -1,6 +1,6 @@
 /**
  * @param length How many random entries to use. Defaults to 5.
- * @param entries What strings to use for generating the final random string. By default, this is equal to `"A", "a", "B", "b", "0", "1", "2", ...`. You could also use strings with a length greater than 1, e.g. `aa`, `bbb`.
+ * @param entries What strings to pick randomly from. By default, this is equal to `"A", "a", "B", "b", "0", "1", "2", ...`.
  * @param separator How to join the random entries. By default, this is `""`. As an example, you can use `"-""` as a separator to generate strings like `"a-Z-9-b-F"`.
  * @example
  * ```ts
@@ -8,4 +8,4 @@
  * // ---> "cat-dog-cat-cat"
  * ```
  */
-export default function(length: number, entries: string[], separator: string): string;
+export default function(length?: number, entries?: string[], separator?: string): string;
